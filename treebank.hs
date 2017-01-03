c76bef5d3bd63fab49fe1a0dd854ef6617735d59
Manu Quadros <emanuel.quadros@yale.edu>
2017-01-03 14:52:29 -0500

    descendants :: TreePos Full a -> [TreePos Full a]

--- treebank.hs
+++ treebank.hs
@@ -1,17 +1,52 @@
 import NLP.PennTreebank
+--import Control.Lens
 import Data.Tree
+import Data.List
+--import Data.Tree.Lens hiding (children)
+import Data.Tree.Zipper
 import Data.String.Utils
-import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec hiding (label)
+import Text.Parsec.String
 import System.Environment
 import Data.List
 
--- getTree 
-    
+
+type Label = String
+
+-- Wrapper around the parser, calling it as many times as possible on
+-- a string, and returning a list.
+parseText :: String -> [Tree String]
+parseText text = case parse (many parseTree) "wsj" (strip text) of
+                   Left err -> error $ "Input:\n" ++ show text ++
+                               "\nError:\n" ++ show err
+                   Right result -> result
+
+-- Send the content of a file to parseText
+parseFile :: FilePath -> IO [Tree String]
+parseFile f = do
+  text <- readFile f
+  return $ parseText (strip text)
+
+-- Search function
+--search :: Label -> Tree String -> [TreePos Full a]
+--search l tr = go . fromTree where
+--    go loc = filter (eqLabel l . tree) children
+--             (foldl1 (++) loc:(map go (childrenAsList loc)))
+
+eqLabel :: Label -> Tree String -> Bool
+eqLabel l tr = rootLabel tr == l
+
+-- get all descendants of a certain position in the tree, where `descendant-of'
+-- is taken to be a reflexive relation.
+descendants :: TreePos Full a -> [TreePos Full a]
+descendants pos
+    | hasChildren pos = pos:foldl1 (++) (map descendants kids)
+    | otherwise = []
+    where
+      kids = map fromTree (forest (children pos))
+
+
 main = do
   input <- getArgs
   wsj <- (readFile (head input))
-  let tree = case parse parseTree "wsj" (strip wsj) of
-               Left err -> error $ "Input:\n" ++ show wsj ++
-                           "\nError:\n" ++ show err
-               Right result -> result
-  putStrLn "DEBUG: parsed:"; putStrLn (drawTree tree)
+  putStrLn "DEBUG: parsed:"
